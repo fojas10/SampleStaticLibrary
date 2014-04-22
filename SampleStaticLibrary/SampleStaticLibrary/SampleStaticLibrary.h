4a7ade487566dc5015b19a1c8caa451cfc7ae177
egay <edgar.fojas@gmail.com>
2014-04-22 13:22:54 +0800

    Created project

---
+++ SampleStaticLibrary/SampleStaticLibrary/SampleStaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  SampleStaticLibrary.h
+//  SampleStaticLibrary
+//
+//  Created by Edgar Allan Fojas on 4/22/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleStaticLibrary : NSObject
+
+@end
---
+++ SampleStaticLibrary/SampleStaticLibrary/SampleStaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  SampleStaticLibrary.m
+//  SampleStaticLibrary
+//
+//  Created by Edgar Allan Fojas on 4/22/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import "SampleStaticLibrary.h"
+
+@implementation SampleStaticLibrary
+
+@end
