729eb033d66ffa0f5608faca83e064cf4d0af008
egay <edgar.fojas@gmail.com>
2014-07-24 17:33:29 +0800

    minor changes

---
+++ SampleStaticLibrary/SDK/SampleStaticLibrary/Header2.h
@@ -0,0 +1,15 @@
+//
+//  Header2.h
+//  SampleStaticLibrary
+//
+//  Created by Edgar Allan Fojas on 7/24/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SubHeaders/Header3.h"
+
+@interface Header2 : NSObject
+
+@end
---
+++ SampleStaticLibrary/SDK/SampleStaticLibrary/SubHeaders/Header3.h
@@ -0,0 +1,13 @@
+//
+//  Header3.h
+//  SampleStaticLibrary
+//
+//  Created by Edgar Allan Fojas on 7/24/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Header3 : NSObject
+
+@end
